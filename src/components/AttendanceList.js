330ad6a76d37887e2eed908aad7c2550d2ec1714
bigmosi <kinyeramo@gmail.com>
2023-06-23 21:00:25 +0300

    added attendance list

--- src/components/AttendanceList.js
+++ src/components/AttendanceList.js
@@ -1,87 +1,64 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import './AttendanceList.css';
 
-const AttendancePage = () => {
-  const { id } = useParams();
-  const [attendance, setAttendance] = useState([]);
-  const [newAttendance, setNewAttendance] = useState({
-    date: '',
-    status: '',
-  });
+const AttendanceList = () => {
+  const [attendanceList, setAttendanceList] = useState([]);
+  const [studentNames, setStudentNames] = useState({});
 
   useEffect(() => {
-    fetchAttendance();
+    fetchAttendanceList();
   }, []);
 
-  const fetchAttendance = async () => {
+  const fetchAttendanceList = async () => {
     try {
-      const response = await axios.get(`http://localhost:8080/api/students/${id}/attendance`);
-      console.log(response.data);
-      setAttendance(response.data);
+      const response = await axios.get('http://localhost:8080/api/attendance');
+      setAttendanceList(response.data);
+      fetchStudentNames(response.data);
     } catch (error) {
-      console.error('Error fetching attendance:', error);
+      console.error('Error fetching attendance list:', error);
     }
   };
 
-  const handleAttendanceSubmit = async (event) => {
-    event.preventDefault();
+  const fetchStudentNames = async (attendanceData) => {
+    const studentIds = attendanceData.map(attendance => attendance.student);
+    const uniqueStudentIds = [...new Set(studentIds)];
+
     try {
-      const response = await axios.post(`http://localhost:8080/api/students/${id}/attendance`, newAttendance);
-      setAttendance([...attendance, response.data]);
-      setNewAttendance({
-        date: '',
-        status: '',
+      const response = await axios.get(`http://localhost:8080/api/students?ids=${uniqueStudentIds.join(',')}`); // Replace '/api/students/names' with your actual student API endpoint
+      const namesMap = {};
+      response.data.forEach(student => {
+        namesMap[student.id] = student.name;
       });
+      setStudentNames(namesMap);
     } catch (error) {
-      console.error('Error adding attendance:', error);
+      console.error('Error fetching student names:', error);
     }
-  };
-
-  const handleAttendanceChange = (event) => {
-    setNewAttendance({
-      ...newAttendance,
-      [event.target.name]: event.target.value,
-    });
   };
 
   return (
     <div>
-      <h2>Attendance Page</h2>
-      <div>
-        <h2>Attendance Records</h2>
-        {attendance.length > 0 ? (
-          <ul>
-            {attendance.map((record) => (
-              <li key={record.id}>
-                <strong>Date:</strong> {record.date} - <strong>Status:</strong> {record.status}
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <div>No attendance records found.</div>
-        )}
-      </div>
-      <div>
-        <h2>Add Attendance</h2>
-        <form onSubmit={handleAttendanceSubmit}>
-          <div>
-            <label>Date:</label>
-            <input type="date" name="date" value={newAttendance.date} onChange={handleAttendanceChange} />
-          </div>
-          <div>
-            <label>Status:</label>
-            <select name="status" value={newAttendance.status} onChange={handleAttendanceChange}>
-              <option value="">Select Status</option>
-              <option value="Present">Present</option>
-              <option value="Absent">Absent</option>
-            </select>
-          </div>
-          <button type="submit">Add</button>
-        </form>
-      </div>
+      <h2>Attendance</h2>
+      <table className="attendance-table">
+        <thead>
+          <tr>
+            <th>Student</th>
+            <th>Date</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {attendanceList.map(attendance => (
+            <tr key={attendance.id}>
+              <td>{studentNames[attendance.student]}</td>
+              <td>{attendance.date}</td>
+              <td>{attendance.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
 
-export default AttendancePage;
+export default AttendanceList;
