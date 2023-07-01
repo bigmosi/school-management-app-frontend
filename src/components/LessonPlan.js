369329f34e86cafaad563aa2eae4288aeb080ec6
bigmosi <kinyeramo@gmail.com>
2023-07-01 21:08:56 +0300

    added lesson plan form

--- src/components/LessonPlan.js
+++ src/components/LessonPlan.js
@@ -1,41 +1,98 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const LessonPlan = () => {
-    const [title, setTitle] = useState("");
-    const [descripton, setDescription] = useState("");
-    const [file, setFile] = useState(null);
-    const [teacher, setTeacher] = useState("");
-    const [success, setSuccess] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [file, setFile] = useState(null);
+  const [teacher, setTeacher] = useState("");
+  const [teachers, setTeachers] = useState([]);
+  const [success, setSuccess] = useState("");
 
-    const handleFileChange = (e) => {
-        setFile(e.target.files[0]);
+  useEffect(() => {
+    fetchTeachers();
+  }, []);
+
+  const fetchTeachers = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/teachers");
+      setTeachers(response.data);
+    } catch (error) {
+      console.error("Error fetching teachers", error);
     }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
-        const formData = new FormData();
-        formData.append('title', title);
-        formData.append('description', descripton);
-        formData.append('file', file);
-        formData.append('teacher', teacher);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            const response = await axios.post('http://localhost:8080/api/lesson-plans/', formData);
-            console.log('Lesson plan created:', (response).data);
-            setTimeout(() => {
-                setSuccess("Lesson plan submitted successfully.");
-            }, 5000);
-        } catch (error) {
-            console.error('Error creating lesson plan', error);
-        }
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("description", description);
+    formData.append("file", file);
+    formData.append("teacher", teacher);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/lesson-plans/",
+        formData
+      );
+      console.log("Lesson plan created:", response.data);
+      setSuccess("Lesson plan submitted successfully.");
+    } catch (error) {
+      console.error("Error creating lesson plan", error);
     }
-    return (
+  };
+
+  return (
+    <div>
+      <h2>Create Lesson Plan</h2>
+      <form onSubmit={handleSubmit}>
         <div>
-            <h2>Create Lesson Plan</h2>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
         </div>
-    );
-}
+        <div>
+          <label htmlFor="description">Description:</label>
+          <textarea
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+        </div>
+        <div>
+          <label htmlFor="file">File:</label>
+          <input id="file" type="file" onChange={handleFileChange} />
+        </div>
+        <div>
+          <label htmlFor="teacher">Teacher:</label>
+          <select
+            id="teacher"
+            value={teacher}
+            onChange={(e) => setTeacher(e.target.value)}
+          >
+            <option value="">Select a teacher</option>
+            {teachers.map((teacher) => (
+              <option key={teacher._id} value={teacher._id}>
+                {teacher.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button type="submit">Create</button>
+      </form>
+      {success && <p>{success}</p>}
+    </div>
+  );
+};
 
 export default LessonPlan;
