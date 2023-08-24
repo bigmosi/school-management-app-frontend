661caf1c768af1d7f26ce8e308aa3355fa7e11d2
kidde60 <kgeorgewilliam60@gmail.com>
2023-08-24 18:02:57 +0300

    Add login to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Home.css';
+import React from "react";
+import LoginForm from "../components/LoginForm";
+
+// import { Link } from 'react-router-dom';
+import "./Home.css";
 
 const HomePage = () => {
   return (
     <div className="homepage">
       <h1>Welcome to new way University of Eduma</h1>
-      <p>Manage your school efficiently with our comprehensive school management app.</p>
+      <p>
+        Manage your school efficiently with our comprehensive school management
+        app.
+      </p>
       <h3>How to enroll your child to a class ?</h3>
+
+      <LoginForm />
     </div>
   );
 };
